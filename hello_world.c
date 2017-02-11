4c6271d8449bbb0463a325d5eacce11e1502d6b6
rrodenbu <riley.rodenburg@me.com>
2017-02-11 09:29:17 -0500

    First piece of practice.

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+#import <stdio.h>
+
+// to run:
+// open terminal
+// go to file in home directory: dropbox > misc > c-practice
+// type: make hello_world
+// type: ./hello_world
+// DONE.
+
+int main()
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
